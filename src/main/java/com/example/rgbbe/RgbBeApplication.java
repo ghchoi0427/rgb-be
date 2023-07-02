04cb33d849a9587a1dda24d519bf68e40da3f344
ghchoi <ghchoi0427@gmail.com>
2023-07-02 16:22:00 +0900

    Initial commit

---
+++ src/main/java/com/example/rgbbe/RgbBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rgbbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgbBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RgbBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rgbbe/RgbBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rgbbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgbBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
